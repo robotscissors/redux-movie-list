810d94d169acfa89f5c55de03164a268dd65ebf8
Juan Lucero <luceroweb@gmail.com>
2021-07-31 09:55:48 -0700

    Cleaned up console log statements

--- src/redux/enhancers/monitorReducerEnhancer.js
+++ src/redux/enhancers/monitorReducerEnhancer.js
@@ -3,12 +3,10 @@ const round = number => Math.round(number * 100) / 100;
 const monitorReducerEnhancer =
   createStore => (reducer, initialState, enhancer) => {
     const monitoredReducer = (state, action) => {
-      const start = performance.now()
+      // const start = performance.now()
       const newState = reducer(state, action)
-      const end = performance.now()
-      const diff = round(end - start)
-
-      console.log('reducer process time:', diff)
+      // const end = performance.now()
+      // const diff = round(end - start)
 
       return newState
     }
--- src/redux/middleware/logger.js
+++ src/redux/middleware/logger.js
@@ -2,7 +2,6 @@ const logger = store => next => action => {
     console.group(action.type)
     console.info('dispatching', action)
     let result = next(action)
-    console.log('next state', store.getState())
     console.groupEnd()
     return result
 }
