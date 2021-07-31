1468d18bc9db6960552a923e95ac749913521c8d
Juan Lucero <luceroweb@gmail.com>
2021-07-31 09:20:30 -0700

    Made navbar sticky

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,14 +4,23 @@ import Nav from 'react-bootstrap/Nav';
 
 const Navbar = () => {
     return (
-        <RBNavbar bg="light">
+        <RBNavbar bg="light" fixed="top" expand="sm">
           <Container>
-            <RBNavbar.Brand href="/">Movie Database</RBNavbar.Brand>
+            <RBNavbar.Brand href="/redux-movie-list/">
+              <img
+                src="/redux-movie-list/logo192.png"
+                width="30"
+                height="30"
+                alt=""
+                className="d-inline-block align-top mr-2"
+              />
+              Movie Database
+            </RBNavbar.Brand>
             <RBNavbar.Toggle />
               <RBNavbar.Collapse>
                 <Nav>
-                  <Nav.Link href="/">Home</Nav.Link>
-                  <Nav.Link href="/movie-list">Movie List</Nav.Link>
+                  <Nav.Link href="/redux-movie-list/">Home</Nav.Link>
+                  <Nav.Link href="/redux-movie-list/movie-list">Movie List</Nav.Link>
                 </Nav>
               </RBNavbar.Collapse>
           </Container>
