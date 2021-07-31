6644129de5afea277ed987e227c713fd7449269b
Juan Lucero <luceroweb@gmail.com>
2021-07-30 19:47:38 -0700

    styled nav

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,11 +1,22 @@
 import { Link } from 'react-router-dom';
+import RBNavbar from 'react-bootstrap/Navbar';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
 
 const Navbar = () => {
     return (
-        <div>
-            <Link to="/">Home</Link>
-            <Link to="/movie-list">Movie List</Link>
-        </div>
+        <RBNavbar bg="light">
+          <Container>
+            <RBNavbar.Brand href="/">Movie Database</RBNavbar.Brand>
+            <RBNavbar.Toggle />
+              <RBNavbar.Collapse>
+                <Nav>
+                  <Nav.Link href="/">Home</Nav.Link>
+                  <Nav.Link href="/movie-list">Movie List</Nav.Link>
+                </Nav>
+              </RBNavbar.Collapse>
+          </Container>
+        </RBNavbar>
     );
 }
 
