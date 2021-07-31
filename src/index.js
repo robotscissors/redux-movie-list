e1900b732358d4cb4993e5e1f452ef94c8c8e5ae
Juan Lucero <luceroweb@gmail.com>
2021-07-31 09:20:11 -0700

    Fixed navbar paths

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import MovieSearch from './components/MovieSearch';
 import configureStore from './redux/store';
 import Navbar from './components/Navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import './index.css';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
     <Router>
       <Navbar/> 
       <Switch>
-        <Route exact path="/" component={MovieSearch} />
-        <Route exact path="/movie-list" component={App} />
+        <Route exact path="/redux-movie-list/" component={MovieSearch} />
+        <Route exact path="/redux-movie-list/movie-list" component={App} />
       </Switch>
     </Router>
   </Provider>,
