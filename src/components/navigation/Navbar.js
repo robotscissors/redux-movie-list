ca9168001fa756321dfa269dc44685a9fafaa6be
Juan Lucero <luceroweb@gmail.com>
2021-08-01 22:29:19 -0700

    Refactored BrowserRouter to HashRouter

--- src/components/navigation/Navbar.js
+++ src/components/navigation/Navbar.js
@@ -1,30 +1,31 @@
 import RBNavbar from 'react-bootstrap/Navbar';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
     return (
-        <RBNavbar bg="light" fixed="top" expand="sm">
-          <Container>
-            <RBNavbar.Brand href="/redux-movie-list">
-              <img
-                src="/redux-movie-list/logo192.png"
-                width="30"
-                height="30"
-                alt=""
-                className="d-inline-block align-top mr-2"
-              />
-              Movie Database
-            </RBNavbar.Brand>
-            <RBNavbar.Toggle />
-              <RBNavbar.Collapse>
-                <Nav>
-                  <Nav.Link href="/redux-movie-list">Home</Nav.Link>
-                  <Nav.Link href="/redux-movie-list/movie-list">Movie List</Nav.Link>
-                </Nav>
-              </RBNavbar.Collapse>
-          </Container>
-        </RBNavbar>
+      <RBNavbar bg="light" fixed="top" expand="sm">
+        <Container>
+          <RBNavbar.Brand as={Link} href="/">
+            <img
+              src="/redux-movie-list/logo192.png"
+              width="30"
+              height="30"
+              alt=""
+              className="d-inline-block align-top mr-2"
+            />
+            Movie Database
+          </RBNavbar.Brand>
+          <RBNavbar.Toggle />
+          <RBNavbar.Collapse>
+            <Nav>
+              <Nav.Link as={Link} to="/">Home</Nav.Link>
+              <Nav.Link as={Link} to="/movie-list">Movie List</Nav.Link>
+            </Nav>
+          </RBNavbar.Collapse>
+        </Container>
+      </RBNavbar>
     );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { Provider } from 'react-redux';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { HashRouter, Switch, Route } from 'react-router-dom';
 import ReactDOM from 'react-dom';
 import MovieList from './components/MovieList';
 import MovieSearchResults from './components/MovieSearchResults';
@@ -11,13 +11,13 @@ import './index.css';
 
 ReactDOM.render(
   <Provider store={configureStore()}>
-    <Router>
+    <HashRouter>
       <Navbar/> 
       <Switch>
-        <Route exact path="/redux-movie-list" component={MovieSearchResults} />
-        <Route exact path="/redux-movie-list/movie-list" component={MovieList} />
+        <Route exact path="/" component={MovieSearchResults} />
+        <Route path="/movie-list" component={MovieList} />
       </Switch>
-    </Router>
+    </HashRouter>
   </Provider>,
   document.getElementById('root')
 );
